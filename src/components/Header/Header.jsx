2d6f897c238057fd2cee2d79bd20fe8ce6c0282c
Truong Thanh <truongthanhh88@gmail.com>
2024-09-26 20:08:02 +0700

    "update sticky"

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import OptionMenu from "../OptionMenu/OptionMenu";
 const optionsArray = ["Option 1", "Option 2", "Option 3"];
 export default function Header() {
+  const [opacity, setOpacity] = useState(1);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const scrollPosition = window.scrollY;
+      const newOpacity = Math.max(0.8, 1 - scrollPosition / 100);
+      setOpacity(newOpacity);
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
+    };
+  }, []);
   return (
     <>
-      <div class="flex items-center bg-white mx-[132px]">
+      <div
+        class=" sticky top-0 w-full flex items-center bg-white mx-[132px] z-20  "
+        style={{ opacity: opacity }}
+      >
         <div class="flex items-center flex-shrink-0 text-blue-500 mr-10">
           <span class="font-extrabold  text-2xl tracking-tight ">
             Koi Express
